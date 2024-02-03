690f2ea5a2fb790e0b516a963841345b839066ce
Balint Orosz <orosz.balint.97@gmail.com>
2024-02-03 15:27:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Boot
+
+```
+curl https://raw.githubusercontent.com/blntrsz/boot/main/boot.sh | sh
+```
---
+++ boot.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+# nvim
+if [ -d ~/.config/nvim ]; then
+  mv ~/.config/nvim ~/.config/nvim.bak
+fi
+git clone git@github.com:blntrsz/nvim.git ~/.config/nvim
+
+# tmux
+if [ -d ~/.config/tmux ]; then
+  mv ~/.config/tmux ~/.config/tmux.bak
+fi
+git clone git@github.com:blntrsz/tmux.git ~/.config/tmux
+
+# wezterm
+if [ -d ~/.config/wezterm ]; then
+  mv ~/.config/wezterm ~/.config/wezterm.bak
+fi
+git clone git@github.com:blntrsz/wezterm.git ~/.config/wezterm
+
