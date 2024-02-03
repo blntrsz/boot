d5be4254a82e32c683c8775192a5f5e23405ef5d
Balint Orosz <orosz.balint.97@gmail.com>
2024-02-03 15:36:08 +0100

    add tpm

--- boot.sh
+++ boot.sh
@@ -11,6 +11,7 @@ if [ -d ~/.config/tmux ]; then
   mv ~/.config/tmux ~/.config/tmux.bak
 fi
 git clone git@github.com:blntrsz/tmux.git ~/.config/tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # wezterm
 if [ -d ~/.config/wezterm ]; then
